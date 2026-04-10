4dab4a00ccd92bd984d620868b44a8c1a9429260
t0ll1v3r <jackson.clary@c2mg.org>
2026-04-10 17:27:24 -0400

    added example code

--- skills/cluster-3/21/code/main/station_example_main.c
+++ skills/cluster-3/21/code/main/station_example_main.c
@@ -81,7 +81,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
             s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
         } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            xEventGroupSetBits(s_wifi_event_group, WzzIFI_FAIL_BIT);
         }
         ESP_LOGI(TAG,"connect to the AP fail");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
